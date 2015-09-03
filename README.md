1283f4cb9b04176d3603ae56b2aa62b69e0a605d
Cirych <cir_reg@mail.ru>
2015-09-03 21:46:38 +0300

    Final

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # freecamp-WikipediaViewer
 Wikipedia_Viewer for freeCodeCamp<br>
-http://codepen.io/Cirych/full/
+http://codepen.io/Cirych/full/bVNqgm/
--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-angular.module('WikipediaViewer', ['ngMaterial'])
-.controller('AppCtrl', ['$scope','$http',function($scope,$http) {
-  this.Tiles = (function() {
-    var tiles = [];
-	$http.get('http://www.freecodecamp.com/news/hot').success(function(data){
-		data.forEach(function(item){
-			tiles.push({
-			title: item.headline,
-			});
-		});
-	});
-	
-    return tiles;
-  })();
-  
-
-
-}]);
+(function () {
+  'use strict';
+  angular
+      .module('WikipediaViewer', ['ngMaterial'])
+      .controller('AppCtrl', AppCtrl);
+  function AppCtrl ($http, $log, $q) {
+    var self = this;
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+	self.randomGet   = randomGet;
+	self.result = [];
+	self.params_s = {action:'query',generator:'search',format:'json',callback:'JSON_CALLBACK'};
+	self.params_f = {action:'query',generator:'search',format:'json',prop:'pageimages|extracts',pilimit:'max',exintro:true,explaintext:true,exsentences:1,exlimit:'max',callback:'JSON_CALLBACK'};
+	self.params_r = {action:'query',generator:'random',grnnamespace:0,grnlimit:10,prop:'pageimages|extracts',pilimit:'max',exintro:true,explaintext:true,exsentences:1,exlimit:'max',format:'json',callback:'JSON_CALLBACK'};
+	self.req = {
+			method:'jsonp',
+			url:'http://en.wikipedia.org/w/api.php',
+			params:self.params_s
+		};
+ 
+    function querySearch(query,params) {
+		var deferred = $q.defer();
+		self.req.params = params||self.params_s;
+		self.req.params.gsrsearch = query;
+		var wikiDataString = 'http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='+ query +'&format=json&callback=JSON_CALLBACK';
+		$http(self.req).then(function(response) {deferred.resolve(response.data.query.pages);});
+		return deferred.promise;
+    };
+    function searchTextChange(text) {
+      //$log.info('Text changed to ' + text);
+    };
+    function selectedItemChange(item) {
+      //$log.info('Item changed to ' + JSON.stringify(item));
+	  if(item) querySearch(item.title,self.params_f).then(function(messages) {self.result = messages});
+    };
+	function randomGet() {
+		querySearch(null,self.params_r).then(function(messages) {self.result = messages});
+    };
+  };
+})();
